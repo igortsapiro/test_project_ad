69765417f9f7d739dca39d45d10d2c044093e0a6
Igor Spenk <igorspenk@gmail.com>
2019-04-26 10:22:35 +0300

    added main migrations and model (Ad, User models)

--- database/migrations/2019_04_25_191937_create_ads_table.php
+++ database/migrations/2019_04_25_191937_create_ads_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateAdsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,17 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
+        Schema::create('ads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('title');
+            $table->text('description');
             $table->timestamp('created_at')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users');
+
         });
+
     }
 
     /**
@@ -27,6 +33,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('ads');
     }
 }
